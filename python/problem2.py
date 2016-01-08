14c041049e0805f7348fcf60638a9bab843c4415
Wef2 <qhrud5725@hanmail.net>
2016-01-08 14:34:17 +0900

    Update problem2.py

---
+++ python/problem2.py
@@ -0,0 +1,23 @@
+result = 0
+index = 0
+true = 1
+
+
+def fibonacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n > 2:
+        return fibonacci(n - 2) + fibonacci(n - 1)
+
+
+while true == 1:
+    index += 1
+    if fibonacci(index) < 4000000:
+        if fibonacci(index) % 2 == 0:
+            result += fibonacci(index)
+    else:
+        true = 0
+
+print(result)
