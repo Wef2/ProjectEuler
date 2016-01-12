2210254a91e9b1592f5a3f4e5b6b7ef321de3575
Wef2 <qhrud5725@hanmail.net>
2016-01-12 17:10:20 +0900

    Refactoring

--- python/problem030.py
+++ python/problem030.py
@@ -1,10 +1,9 @@
 result = 0
 
 for i in range(2, 354295):
-    str_i = str(i)
     value = 0
-    for a in range(0, len(str_i)):
-        num = int(str_i[a:a+1])
+    for a in str(i):
+        num = int(a)
         value += num**5
     if value == i:
         result += i
--- python/problem040.py
+++ python/problem040.py
@@ -4,11 +4,10 @@ total_length = 0
 result = 1
 
 while True:
-    str_index = str(index)
-    for i in range(0, len(str_index)):
+    for i in str(index):
         total_length += 1
         if total_length == product_position:
-            result *= int(str_index[i:i + 1])
+            result *= int(i)
             product_position *= 10
     if product_position == 10000000:
         break
