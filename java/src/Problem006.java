1195ed81b366e4bd10b6fa8967a275820e6d7615
Wef2 <qhrud5725@hanmail.net>
2016-02-05 13:50:20 +0900

    Update Problem006.java

--- java/src/Problem006.java
+++ java/src/Problem006.java
@@ -4,19 +4,18 @@
 public class Problem006 {
 
 
-
-    int result(){
+    int result() {
         int value1 = 0;
         int value2 = 0;
-        for(int i=1; i<= 100; i++){
-            value1 = value1 + i*i;
+        for (int i = 1; i <= 100; i++) {
+            value1 = value1 + i * i;
             value2 = value2 + i;
         }
         value2 = value2 * value2;
         return value2 - value1;
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Problem006 problem = new Problem006();
         System.out.println(problem.result());
     }
