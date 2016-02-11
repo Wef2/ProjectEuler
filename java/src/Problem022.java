8a6d816ec2fc8a8460706d9c5ca6b55bbdf878c4
Wef2 <qhrud5725@hanmail.net>
2016-02-11 23:10:44 +0900

    Update Problem022.java

---
+++ java/src/Problem022.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Problem022 {
+
+    private Scanner scanner;
+    private File file;
+    private List<String> list = new ArrayList();
+
+    int score(String name, int order) {
+        int value = 0;
+        for (int a = 0; a < name.length(); a++) {
+            value = value + name.charAt(a) - 64;
+        }
+        return value * order;
+    }
+
+    long result() {
+        init();
+        long resultValue = 0;
+        for (int i = 0; i < list.size(); i++) {
+            resultValue = resultValue + score(list.get(i), i + 1);
+        }
+        return resultValue;
+    }
+
+    void init() {
+        file = new File("../textfiles/problem022.txt");
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String text = scanner.next();
+        StringTokenizer stringTokenizer = new StringTokenizer(text, ",");
+        while (stringTokenizer.hasMoreTokens()) {
+            String str = stringTokenizer.nextToken();
+            str = str.substring(1, str.length() - 1);
+            list.add(str);
+        }
+        Collections.sort(list);
+    }
+
+    public static void main(String args[]) {
+        Problem022 problem = new Problem022();
+        System.out.println(problem.result());
+    }
+
+}
